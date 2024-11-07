c08e513e6fb0100efd07fb67578bb46dd49c53aa
son phoenix <sonphoenix2002@gmail.com>
2024-11-07 16:47:18 +0100

    second commit to the module

--- models/emprunt.py
+++ models/emprunt.py
@@ -3,18 +3,21 @@ from odoo import fields, models, api
 class Emprunt(models.Model):
     _name = 'emprunt'
 
-    date_debut = fields.Date(string='Date de debut', required=True, default=fields.Date.context_today, readonly=True)
+    date_debut = fields.Date(string='Date de debut', required=True, compute='_compute_date_debut')
     date_fin = fields.Date(string='Date de fin', required=True)
 
     rendu = fields.Boolean(string='Rendu', compute='_compute_rendu')
 
     emprunteur_id = fields.Many2one('emprunteur', string='Emprunteur', required=True)
-    empruntas_ids = fields.One2many('empruntas', 'emprunt_id', string='Lignes d\'emprunt', required=True)
+    empruntas_ids = fields.One2many('empruntas', 'emprunt_id', string="Lignes d'emprunt", required=True)
+
+    @api.depends('date_debut')
+    def _compute_date_debut(self):
+        for record in self:
+            if not record.date_debut:
+                record.date_debut = fields.Date.today()
 
     @api.depends('date_fin')
     def _compute_rendu(self):
         for record in self:
-            if record.date_fin < fields.Date.today():
-                record.rendu = True
-            else:
-                record.rendu = False
+            record.rendu = bool(record.date_fin and record.date_fin < fields.Date.today())
